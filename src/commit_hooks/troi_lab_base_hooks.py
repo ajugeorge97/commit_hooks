0cbc18b43b1a69a8463fcb7759b8a9a6eab87789
Aju George <Aju.George@uk-erlangen.de>
2026-03-16 14:17:21 +0100

    chore: rename variable

--- src/commit_hooks/troi_lab_base_hooks.py
+++ src/commit_hooks/troi_lab_base_hooks.py
@@ -4,8 +4,8 @@ import sys
 
 
 def troi_lab_base_hooks():
-    root_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
-    cfg = os.path.join(root_path, ".pre-commit-config.yaml")
+    config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
+    cfg = os.path.join(config_path, ".pre-commit-config.yaml")
     cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
     os.execvp(cmd[0], cmd)
     sys.exit(0)
