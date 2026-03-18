986e7eebf04d74abe1269aeccbbb76089f9e8b64
Aju George <Aju.George@uk-erlangen.de>
2026-03-18 09:33:15 +0100

    fix: config filename

--- src/commit_hooks/troi_lab_base_hooks.py
+++ src/commit_hooks/troi_lab_base_hooks.py
@@ -5,7 +5,7 @@ import subprocess
 
 def troi_lab_base_hooks():
     config_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), "templates","configs")
-    cfg = os.path.join(config_path, ".pre-commit-config.yaml")
+    cfg = os.path.join(config_path, ".pre-commit-cfg.yaml")
     cmd = ["pre-commit", "run", "--config", cfg, "--files"] + sys.argv[1:]
     result = subprocess.run(cmd)
     sys.exit(result.returncode)
